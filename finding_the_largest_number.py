2dc39a1927dbfd6e3d266f73284dbd1236991c2f
Uella Blainne Fabregas <uellablainneffabregas@iskolarngbayan.pup.edu.ph>
2024-01-11 09:18:46 +0800

    creating the message box function

--- finding_the_largest_number.py
+++ finding_the_largest_number.py
@@ -60,14 +60,54 @@ class CanvasAndButton (tk.Frame):
           command=lambda: print("button_2 clicked"),
           relief="flat"
       )
-    self.button_2.place(
+      self.button_2.place(
         x=68,
         y=515,
         width=279,
         height= 128
     )
+      
+class InputPage(tk.Frame):
+    def __init__(self, parent, controller):
+        BackgroundFrame.__init__(self,parent, "Image_5.png")
+        self.controller=controller
 
+        entry_width = 20
+        pady_value = 80
 
+        y_position1 = 0.5 * self.winfo_reqheight() - 0.9 * pady_value
+        y_position1 = 0.5 * self.winfo_reqheight() + 0.23 * pady_value
+        y_position1 = 0.5 * self.winfo_reqheight() + 1.4 * pady_value
+
+        self.num_entry1 = tk.Entry(self, width = entry_width, justify = tk.CENTER)
+        self.num_entry2 = tk.Entry(self, width = entry_width, justify = tk.CENTER)
+        self.num_entry3 = tk.Entry(self, width = entry_width, justify = tk.CENTER)
+
+        self.num_entry1.place (relx=0.5, rely = 0.5, anchor = tk.CENTER, y=y_position1)
+        self.num_entry2.place (relx=0.5, rely = 0.5, anchor = tk.CENTER, y=y_position2)
+        self.num_entry3.place (relx=0.5, rely = 0.5, anchor = tk.CENTER, y=y_position3)
+        
+        tk.Button(
+            self, 
+            text= "Find", 
+            command= self.find_numbers).place(
+                relx=0.5, 
+                rely = 0.85, 
+                relheight = 0.05, 
+                relwidth = 0.2, 
+                anchor = "center"
+                )
+        self.canvas_and_button = CanvasAndButton (self)
+
+    def find_numbers(self):
+        try:
+            numbers = [float (entry.get()) for entry in (self.num_entry1, self.num_entry2, self.num_entry3)]
+        except ValueError:
+            messagebox.showerror("Error","One or more inputs are not valid numbers.")
+            return
+        
+        
+        
 # Making the Main Window
 window = Tk()
 window.title("Largest Number")
